0a52bcb1f29673c036dff7c78755c7fb7361b0a2
kdh95 <ehgks3323@gmail.com>
2024-05-08 20:29:04 +0900

    max depth update

--- Tree/findMaxDepth.py
+++ Tree/findMaxDepth.py
@@ -17,3 +17,22 @@ def maxDepth(root):
             q.append(cur_node.right, cur_depth + 1)
 
     return max_depth
+
+
+
+root = [3, 9, 20, None, None, 15, 7]
+
+class TreeNode:
+    def _init_(self, l=None, r=None, v=0): # 초기화시 left, right 노드에 값이 없으면 None으로 초기화, value 값이 없으면 0으로 초기화
+        self.left = l
+        self.right = r
+        self.value = v
+
+
+root = TreeNode(v=3) # root node 초기화
+root.left = TreeNode(v=9)
+root.right = TreeNode(v=20)
+root.right.left = TreeNode(v=15)
+root.right.right = TreeNode(v=7)
+
+print(maxDepth(root))
