a0ac7c55cef2837c71179f17187e6b5777be51e8
kdh95 <ehgks3323@gmail.com>
2024-06-18 22:44:36 +0900

    climbing stairs update

---
+++ Dynamic Programming/climbing staris.py
@@ -0,0 +1,19 @@
+# Climbing stairs
+
+# 계단을 올라가고 있다. 이 계단의 꼭대기에 도착하려면 n개의 step만큼 올라가야 한다. 한 번 올라갈 때마다
+# 1 step 또는 2 steps 올라갈 수 있다. 꼭대기에 도달하는 방법의 개수는 총 몇가지 일까요?
+
+# 제약조건
+# 1 <= n <= 45
+
+# input : n = 2
+# output : 2
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+
+# input : n = 3
+# output : 3
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
