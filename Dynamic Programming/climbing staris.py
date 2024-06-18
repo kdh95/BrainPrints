06fdd8bbf33b53f1bb9aca90ec580d3df9f65958
kdh95 <ehgks3323@gmail.com>
2024-06-18 23:14:18 +0900

    climbing stairs update

--- Dynamic Programming/climbing staris.py
+++ Dynamic Programming/climbing staris.py
@@ -17,3 +17,51 @@
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
+
+
+# 접근방법 => 완전탐색
+# 1. 크고 복잡한 문제를 하위 문제로 나눈다.
+# 2. 하위 문제에 대한 답을 계산한다.
+# 3. 하위 문제에 대한 답으로 원래 문제에 대한 답을 계산한다.
+
+# 5층으로 갈 수 있는 경우의 수는 몇가지 인가?
+# 4층에서 올라가거나 3층에서 올라가는 경우의 수 2가지 밖에 없다.
+# 따라서 3층까지 올라가는 경우의 수와 4층까지 올라가는 경우의 수를 모두 더하면 되지 않을까?
+# 피보나치 수열과 똑같다.
+
+
+# def cs(n):
+#     if n == 1:
+#         return 1 # 1층을 올라가는 경우의 수
+#     if n == 2:
+#         return 2 # 2층을 올라가는 경우의 수
+#
+#     return cs(n-1) + cs(n-2)
+
+
+# TOP-DOWN 방식 => 재귀를 사용한다.
+memo = {}
+
+def cs(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n not in memo:
+        memo[n] = cs(n-1) + cs(n-2)
+    return memo[n]
+
+
+# BOTTOM-UP 방식 => 반복문을 사용한다.
+# basecase 2층 : 2, 1층 : 1
+memo = {1: 1, 2: 2}
+
+def cs(n):
+
+    for i in range(3, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+
+    return memo[n]
+
+
+
